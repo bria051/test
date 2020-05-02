7fb18698fe8f65f7a74c3a22a1f6bd84459c5598
at02 <bria051@naver.com>
2020-05-02 18:45:13 +0900

    app_9 image add

---
+++ App/app_9.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, redirect, request, url_for, session
+import os
+import shutil
+
+# homework hint: cv2
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def image_select():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('img_select.html')
+
+if __name__ == "__main__":
+    app.run()
